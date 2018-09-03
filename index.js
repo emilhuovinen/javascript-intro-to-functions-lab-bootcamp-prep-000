c7b46289e1fdebd716b1b9db9347ea8d892f818e
emil.huo@gmail.com <emil.huo@gmail.com>
2018-09-03 15:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,19 @@ function logShout(string) {
  
   
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  var lowercase = "hello!"
-  var mixedCase = "Hi there!"
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
   if (string === lowercase) {
     return "I can\'t hear you!"
   } 
