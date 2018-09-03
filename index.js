440cc47ec318851df4bc4c6483f1dcef3f1e67ed
emil.huo@gmail.com <emil.huo@gmail.com>
2018-09-03 15:43:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ function sayHiToGrandma(string) {
   var lowercase = "hello!"
   var mixedCase = "Hi there!"
   if (string === lowercase) {
-    return "I can\'t hear you!"
-  } 
+    return "I can\'t hear you!";
+  } if (string === uppercase) {
+    return "YES INDEED!";
+  } if (string === mixedCase) {
+    return "I love you, too";
+  }
 }
 
 shout("Hello!")
